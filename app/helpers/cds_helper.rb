14d64b41f5e44f88dbddb8886a440e0d6fc528ff
sato-koi <sato.koichi.0404@gmail.com>
2019-03-05 11:06:56 +0900

    画像登録機能

--- app/helpers/cds_helper.rb
+++ app/helpers/cds_helper.rb
@@ -1,2 +1,9 @@
 module CdsHelper
+  def show_cd_image(cd)
+    if cd.image.attached?
+      image_tag cd.image.variant(resize: "300x300"), class: "img-thumbnail"
+    else
+      image_tag "no_image.png", class: "img-thumbnail"
+    end
+  end
 end
--- app/models/cd.rb
+++ app/models/cd.rb
@@ -1,2 +1,17 @@
 class Cd < ApplicationRecord
+  has_one_attached :image
+  attribute :new_image
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :artist, presence: true, length: { maximum: 50 }
+  validates :price, presence: true,
+    numericality: { 
+      only_integer: true,
+      greater_than: 1
+    }
+  validates :release_date, presence: true
+  validates :description, presence: true, length: { maximum: 1000 }
+
+  before_save do
+    self.image = new_image if new_image
+  end
 end
